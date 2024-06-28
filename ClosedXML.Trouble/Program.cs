445b4c030cc42c6414985ee2ba3cd77b16a22147
Malov Gleb <gleb.malov@arsis.ru>
2024-06-28 11:30:27 +0500

    Init

---
+++ ClosedXML.Trouble/Program.cs
@@ -0,0 +1,29 @@
+﻿using ClosedXML.Report;
+
+var resultPath = "C:/Projects/ClosedXML.Trouble/result.xlsx"; //TODO сюда путь до файла
+var templatePath = "C:/Projects/ClosedXML.Trouble/Template.xlsx"; //TODO сюда путь до файла
+var template = new XLTemplate(templatePath);
+
+var dto = new Dto
+{
+    Table1 = Enumerable.Range(0, 5).Select(x => new Table { Name = $"Имя {x}" }).ToArray(),
+    Table2 = Enumerable.Range(0, 5).Select(x => new Table { Name = $"Имя {x}" }).ToArray(),
+    Table3 = Enumerable.Range(0, 5).Select(x => new Table { Name = $"Имя {x}" }).ToArray(),
+};
+
+template.AddVariable(dto);
+template.Generate();
+
+template.SaveAs(resultPath);
+
+public class Dto
+{
+    public Table[] Table1 { get; set; }
+    public Table[] Table2 { get; set; }
+    public Table[] Table3 { get; set; }
+}
+
+public class Table
+{
+    public string Name { get; set; }
+}
